26b7a96a90216699e2bc145b8dd594c8d660d1f9
martin-alba <martin_08_alba@hotmail.com>
2023-11-29 10:32:22 -0300

    feat: components/NavBar

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import NavBar from "./components/NavBar/NavBar.jsx";
+export default function App() {
+  return <div>{<NavBar />}</div>;
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+function NavBar() {
+  return (
+    <header>
+      <div>
+        <h1>Brand</h1>
+        <img src="https://i.imgur.com/igU59U2.png" alt="brand-name" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
